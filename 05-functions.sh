be18cac563264d5aa87c5f49c493d4c7685e3a99
rajendra <akrajendra597@.com>
2025-09-24 20:10:20 +0530

    05-functions.sh

--- 05-functions.sh
+++ 05-functions.sh
@@ -2,15 +2,6 @@
 
 USERID=$(id -u)
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo "$2 ... FAILURE"
-        exit 1
-    else
-        echo "$2 ... SUCCESS"
-    fi
-}
 
 if [ $USERID -ne 0 ]
 then
@@ -38,6 +29,8 @@ then
 else
    echo "installing Git ... SUCCESS"
 fi
+
+
 
 
 
